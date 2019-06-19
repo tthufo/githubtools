719c8eb0aad9cca96c8fb453e80073f275d1e9e8
Andrew <Andrew.tran@innovatube.com>
2019-06-19 14:33:00 +0700

    ahihii

--- src/Home.js
+++ src/Home.js
@@ -236,7 +236,7 @@ class Login extends Component {
   async getCode(response) {
     this.setState({ loading: true })
     try {
-      fetch(`/api/auth?code=` + response)
+      fetch(`/githubtools/api/auth?code=` + response)
       .then(response => response.json())
       .then(token => localStorage.setItem('accessToken', token.token))
       .then(() => this.getUser())
